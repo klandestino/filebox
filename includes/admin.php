6e14b7443ade35be8a20469e519c28133a07b793
spurge <olof@klandestino.se>
2013-01-28 17:27:45 +0100

    Added admin interface

--- includes/admin.php
+++ includes/admin.php
@@ -5,7 +5,65 @@
  */
 class Filebox_Admin {
 
-	public static function __setup() {
+	static public function __setup() {
+		add_action( 'init', array( Filebox_Admin, 'init' ) );
+	}
+
+	static public function init() {
+		if( is_site_admin() ) {
+			add_action( 'admin_init', array( Filebox_Admin, 'admin_page_save' ) );
+			add_action( 'admin_menu', array( Filebox_Admin, 'admin_menu' ) );
+		}
+	}
+
+	/**
+	 * Adds menu item
+	 * @return void
+	 */
+	public static function admin_menu() {
+		add_submenu_page(
+			'options-general.php',
+			__( 'Filebox Settings', 'filebox' ),
+			__( 'Filebox', 'filebox' ),
+			'manage_options',
+			'filebox',
+			array( Filebox_Admin, 'admin_page' )
+		);
+	}
+
+	/**
+	 * Prints an admin page through template
+	 * @return void
+	 */
+	public static function admin_page() {
+		global $settings;
+		$settings = Filebox::get_options();
+		Filebox::get_template( 'filebox-admin-options' );
+	}
+
+		/**
+	 * Receives the posted admin form and saved the settings
+	 * @return void
+	 */
+	public static function admin_page_save() {
+		if( array_key_exists( 'filebox-save', $_POST ) ) {
+			check_admin_referer( 'filebox_admin' );
+			$settings = Filebox::get_options();
+			
+			foreach( $settings as $key => $val ) {
+				if( array_key_exists( $key, $_POST ) ) {
+					$settings[ $key ] = $_POST[ $key ];
+				}
+			}
+
+			update_option( 'filebox_settings', $settings );
+			wp_redirect( add_query_arg( array( 'filebox-updated' => '1' ) ) );
+		} elseif( array_key_exists( 'filebox-updated', $_GET ) ) {
+			add_action( 'admin_notices', create_function( '', sprintf(
+				'echo "<div class=\"updated\"><p>%s</p></div>";',
+				__( 'Settings updated.', 'filebox' )
+			) ) );
+		}
 	}
 
 }
---
+++ templates/filebox-admin-options.php
@@ -0,0 +1,103 @@
+<?php global $settings;
+
+$fields = array(
+	'group-tab' => array(
+		'textfield',
+		__( 'Group tab title', 'filebox' )
+	),
+	'topics-folder-name' => array(
+		'textfield',
+		__( 'Folder name for imported forum attachments' )
+	),
+	'mail-delay' => array(
+		'textfield',
+		__( 'Delay in minutes before e-mails are sent', 'filebox' )
+	),
+	'multiple-mail-messages-subject' => array(
+		'textfield',
+		__( 'E-mail subject when message contains multiple uploads', 'filebox' ),
+		__( '%1$s = blogname, %2$d = number of files', 'filebox' )
+	),
+	'file-update-mail-subject-single' => array(
+		'textfield',
+		__( 'E-mail subject for single file updated', 'filebox' ),
+		__( '%1$s = blogname, %2$s = filename, %3$s = folder name, %4$s group name', 'filebox' )
+	),
+	'file-update-mail-message-line' => array(
+		'textarea',
+		__( 'E-mail message for single file updated', 'filebox' ),
+		__( '%1$s = filename, %2$s = folder name, %3$s group name', 'filebox' )
+	),
+	'file-upload-mail-subject-single' => array(
+		'textfield',
+		__( 'E-mail subject for single file upload', 'filebox' ),
+		__( '%1$s = blogname, %2$s = filename, %3$s = folder name, %4$s group name', 'filebox' )
+	),
+	'file-upload-mail-message-line' => array(
+		'textarea',
+		__( 'E-mail message for single file upload', 'filebox' ),
+		__( '%1$s = filename, %2$s = folder name, %3$s group name', 'filebox' )
+	),
+	'file-update-notify-single' => array(
+		'textfield',
+		__( 'Notification for single file updated', 'filebox' ),
+		__( '%1$s = filename, %2$s = folder name, %3$s group name', 'filebox' )
+	),
+	'file-update-notify-multi' => array(
+		'textfield',
+		__( 'Notification for multiple files updated in the same folder and group', 'filebox' ),
+		__( '%1$s = filename, %2$s = folder name, %3$s group name', 'filebox' )
+	),
+	'file-upload-notify-single' => array(
+		'textfield',
+		__( 'Notification for single file uploaded', 'filebox' ),
+		__( '%1$s = filename, %2$s = folder name, %3$s group name', 'filebox' )
+	),
+	'file-upload-notify-multi' => array(
+		'textfield',
+		__( 'Notification for multiple new files uploaded in the same folder and group', 'filebox' ),
+		__( '%1$s = filename, %2$s = folder name, %3$s group name', 'filebox' )
+	),
+	'mail-message-wrap' => array(
+		'textarea',
+		__( 'E-mail message body wrapper', 'filebox' ),
+		__( '%1$s = message bodies', 'filebox' )
+	)
+);
+
+?>
+<div class="wrap">
+	<h2><?php _e( 'Forum Notifier Settings', 'bp-forum-notifier' ); ?></h2>
+	<form action="" method="post">
+		<?php wp_nonce_field( 'bp_forum_notifier_admin' ); ?>
+
+		<table class="form-table">
+			<tbody>
+				<?php foreach( $fields as $field_name => $field ) : ?>
+					<tr>
+						<th scope="row">
+							<label for="<?php echo $field_name; ?>"><?php echo $field[ 1 ]; ?></label>
+						</th>
+						<td>
+							<?php if( $field[ 0 ] == 'textfield' ) : ?>
+								<input id="<?php echo $field_name; ?>" name="<?php echo $field_name; ?>" type="textfield" class="large-text" value="<?php echo esc_attr( $settings[ $field_name ] ); ?>" />
+							<?php elseif( $field[ 0 ] == 'textarea' ) : ?>
+								<textarea id="<?php echo $field_name; ?>" name="<?php echo $field_name; ?>" class="large-text"><?php echo $settings[ $field_name ]; ?></textarea>
+							<?php endif; ?>
+
+							<?php if( array_key_exists( 2, $field ) ) : ?>
+								<br />
+								<?php echo $field[ 2 ]; ?>
+							<?php endif; ?>
+						</td>
+					</tr>
+				<?php endforeach; ?>
+			</tbody>
+		</table>
+
+		<p class="submit clear">
+			<input class="button-primary" name="forum-notifier-save" type="submit" value="<?php echo esc_attr( __( 'Save' ) ); ?>" />
+		</p>
+
+	</form>
+</div>

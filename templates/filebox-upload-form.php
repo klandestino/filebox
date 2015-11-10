93ac9929441c4cd2691ad6cbc83b3e6c425967be
Alexander Berthelsen <alexander.berthelsen@gmail.com>
2015-11-10 18:12:36 +0100

    redirect back to group file archive after succesful upload

--- templates/filebox-upload-form.php
+++ templates/filebox-upload-form.php
@@ -92,6 +92,16 @@
 								}
 							}
 
+							function getQueryVariable(variable) {
+								var query = window.location.search.substring(1);
+								var vars = query.split("&");
+								for (var i=0;i<vars.length;i++) {
+									var pair = vars[i].split("=");
+									if(pair[0] == variable){return pair[1];}
+								}
+								return(false);
+							}
+
 							uploader.bind( 'Init', function( up ) {
 								var uploaddiv = $( '#plupload-upload-ui'  );
 
@@ -228,7 +238,7 @@
 
 									up.refresh();
 								} else {
-									window.location.reload();
+									window.location.href = decodeURIComponent( getQueryVariable('prev_url') );
 								}
 							} );
 
